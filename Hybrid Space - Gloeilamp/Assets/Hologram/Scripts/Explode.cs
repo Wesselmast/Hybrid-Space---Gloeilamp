b98ee9ca1d0da846cf1b3b814be1b4f96f60d948
Wessel Mast <mastwessel@gmail.com>
2018-10-24 08:35:05 +0200

    Hele commit en stuff

---
+++ Hybrid Space - Gloeilamp/Assets/Hologram/Scripts/Explode.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour {
+
+    [SerializeField]
+    private float fuseTime;
+    [SerializeField]
+    private float explosionRadius, explosionForce;
+    [SerializeField]
+    public GameObject explosion;
+
+    Rigidbody rb;
+    GameObject boom;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+        StartCoroutine(LightTheFuses());
+    }
+
+    IEnumerator LightTheFuses() {
+        if (boom != null) Destroy(boom);
+        yield return new WaitForSeconds(fuseTime);
+        boom = Instantiate(explosion, transform.position, Quaternion.identity);
+        Collider[] colliders = Physics.OverlapSphere(gameObject.transform.position, explosionRadius);
+        foreach (var c in colliders) {
+            Rigidbody cRb = c.GetComponent<Rigidbody>();
+            if (cRb != null) {
+                Vector3 moveDir = rb.transform.position - cRb.transform.position;
+                cRb.AddForce(moveDir.normalized * -explosionForce, ForceMode.Impulse);
+            }
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Hybrid Space - Gloeilamp/Assets/Hologram/Scripts/MovePlayers.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayers : MonoBehaviour {
+
+    [SerializeField]
+    private float speed, turnSpeed;
+    [SerializeField]
+    private KeyCode left, right, bomb;
+    [SerializeField]
+    private GameObject bombObj;
+    [SerializeField]
+    private float startBombCooldown;
+
+    private bool rotateLeft, rotateRight = false;
+    private bool hasCollided;
+    private float bombCooldown = 0.0f;
+    private float startYPos;
+
+    private void OnCollisionEnter(Collision other) {
+        if (!hasCollided && other.transform.tag == "ground") {
+            hasCollided = true;
+            startYPos = transform.position.y;
+            StartCoroutine(Run());
+        }
+    }
+
+    IEnumerator Run() {
+        hasCollided = true;
+        while (true) {
+            transform.Translate(0, 0, speed * Time.fixedDeltaTime);
+            if(Input.GetKey(right)) gameObject.transform.localEulerAngles += new Vector3(0, -turnSpeed, 0);
+            if(Input.GetKey(left)) gameObject.transform.localEulerAngles += new Vector3(0, turnSpeed, 0);
+            BombSpawning();
+            if (gameObject.transform.position.y <= startYPos - 100) Destroy(gameObject);
+            yield return null;
+        }
+    }
+
+    void BombSpawning() {
+        if (bombCooldown <= 0.0f && Input.GetKeyDown(bomb)) {
+            GameObject b = Instantiate(bombObj, gameObject.transform.position, Quaternion.identity);
+            b.transform.parent = transform.parent;
+            bombCooldown = startBombCooldown;
+        }
+        else bombCooldown -= Time.fixedDeltaTime;
+    }
+}
