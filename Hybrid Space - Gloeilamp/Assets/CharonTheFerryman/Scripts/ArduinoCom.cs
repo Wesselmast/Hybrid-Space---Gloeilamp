cacda1d62e5adcfbbef2934b0b245c658cf0681d
Wessel Mast <mastwessel@gmail.com>
2018-11-29 10:56:13 +0100

    Auto stash before merge of "master" and "origin/master"

--- Hybrid Space - Gloeilamp/Assets/CharonTheFerryman/Scripts/ArduinoCom.cs
+++ Hybrid Space - Gloeilamp/Assets/CharonTheFerryman/Scripts/ArduinoCom.cs
@@ -6,7 +6,11 @@ using System.IO.Ports;
 public class ArduinoCom : MonoBehaviour {
 
     [HideInInspector]
+<<<<<<< Updated upstream
     public bool left, right, up, barrels = false;
+=======
+    public bool left, right, lantern, up = false;
+>>>>>>> Stashed changes
 
     //this stuff has to be hardcoded, so if you get weird errors check this out
     private SerialPort port = new SerialPort("COM7", 9600);
@@ -14,7 +18,7 @@ public class ArduinoCom : MonoBehaviour {
 
 	void Start () {
         try { port.Open(); }
-        catch (System.Exception e) { }
+        catch (System.Exception e) { Debug.Log("System Error: " + e.ToString()); }
     }
 	
 	void Update () {
@@ -22,9 +26,15 @@ public class ArduinoCom : MonoBehaviour {
             Debug.Log("Input: " + input);
             if (input == 1) left = true;
             else if (input == 2) right = true;
+<<<<<<< Updated upstream
             else if (input == 3) up = true;
             else if (input == 4) barrels = true;
             else { left = false; right = false; barrels = false; up = false; }
+=======
+            else if (input == 3) lantern = true;
+            else if (input == 4) up = true;
+            else { left = false; right = false; lantern = false; up = false; }
+>>>>>>> Stashed changes
         }
 	}
 }
