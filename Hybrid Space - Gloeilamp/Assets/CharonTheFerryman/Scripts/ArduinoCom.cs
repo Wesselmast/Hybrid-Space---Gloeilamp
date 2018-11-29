afea4e7afe22121056b7c20c092b1d410cad003b
Wessel Mast <mastwessel@gmail.com>
2018-11-29 12:26:18 +0100

    Multiplayer scene push for playtesting

--- Hybrid Space - Gloeilamp/Assets/CharonTheFerryman/Scripts/ArduinoCom.cs
+++ Hybrid Space - Gloeilamp/Assets/CharonTheFerryman/Scripts/ArduinoCom.cs
@@ -6,15 +6,7 @@ using System.IO.Ports;
 public class ArduinoCom : MonoBehaviour {
 
     [HideInInspector]
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     public bool left, right, up, barrels = false;
-=======
-    public bool left, right, lantern, up = false;
->>>>>>> Stashed changes
-=======
-    public bool left, right, lantern, up = false;
->>>>>>> Stashed changes
 
     //this stuff has to be hardcoded, so if you get weird errors check this out
     private SerialPort port = new SerialPort("COM7", 9600);
@@ -30,21 +22,9 @@ public class ArduinoCom : MonoBehaviour {
             Debug.Log("Input: " + input);
             if (input == 1) left = true;
             else if (input == 2) right = true;
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
             else if (input == 3) up = true;
             else if (input == 4) barrels = true;
             else { left = false; right = false; barrels = false; up = false; }
-=======
-            else if (input == 3) lantern = true;
-            else if (input == 4) up = true;
-            else { left = false; right = false; lantern = false; up = false; }
->>>>>>> Stashed changes
-=======
-            else if (input == 3) lantern = true;
-            else if (input == 4) up = true;
-            else { left = false; right = false; lantern = false; up = false; }
->>>>>>> Stashed changes
         }
 	}
 }
