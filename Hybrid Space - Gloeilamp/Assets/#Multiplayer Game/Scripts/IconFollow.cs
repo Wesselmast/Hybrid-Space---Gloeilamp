21d74f6b55d59e156926e1fdeb9068a9f10fb51a
Unknown <kevin.absen@hotmail.com>
2019-01-05 15:18:07 +0100

    Map UI + Icons for Balloon

---
+++ Hybrid Space - Gloeilamp/Assets/#Multiplayer Game/Scripts/IconFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class IconFollow : MonoBehaviour {
+    [SerializeField]
+    private Transform iconPoint;
+    [SerializeField]
+    private float yPos = 50f;
+
+    private Transform thisTransform;
+    Camera mapCam;
+
+    private void Start() {
+        thisTransform = transform;
+        mapCam = GameObject.FindWithTag("MapCamera").GetComponent<Camera>();
+    }
+
+    private void FixedUpdate() {
+        transform.position = new Vector3(iconPoint.position.x, yPos, iconPoint.position.z);
+        transform.rotation = Quaternion.LookRotation(mapCam.transform.forward);
+    }
+}
