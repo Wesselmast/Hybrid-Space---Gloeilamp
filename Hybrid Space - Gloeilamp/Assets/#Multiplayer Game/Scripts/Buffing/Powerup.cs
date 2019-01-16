453418d39f91d2d5d7b925609198155c661887e1
Wessel Mast <mastwessel@gmail.com>
2019-01-16 21:31:42 +0100

    Discarded scene changes + FMOD

--- Hybrid Space - Gloeilamp/Assets/#Multiplayer Game/Scripts/Buffing/Powerup.cs
+++ Hybrid Space - Gloeilamp/Assets/#Multiplayer Game/Scripts/Buffing/Powerup.cs
@@ -4,13 +4,21 @@ public class Powerup : MonoBehaviour {
     private enum Player {
         Boat, Balloon
     }
-
+    [SerializeField]
+    private string powerupName;
     [SerializeField]
     private ScriptableBuff scriptableBuff;
     [SerializeField]
     private Player player;
 
     private float startRot;
+    private FMODCom com;
+    private Rigidbody rb;
+
+    private void Awake() {
+        try { com = new FMODCom(powerupName); }
+        catch { }
+    }
 
     private void Start() {
         startRot = transform.eulerAngles.y;
@@ -26,6 +34,7 @@ public class Powerup : MonoBehaviour {
         if (other.tag == player.ToString()) {
             try { other.GetComponent<Buffable>().AddBuff(scriptableBuff); }
             catch { Debug.LogError("Buffable component not found!"); }
+            com.Play3D(transform, rb);
             Destroy(gameObject);
         }
     }
